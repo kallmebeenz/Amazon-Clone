72a0534170b6315ceaca1180bbfe1e3d8c2f3310
unknown <you@example.com>
2024-01-21 07:50:48 +0100

    Updates

---
+++ precache-manifest.c98ffb8f519938b41a1466272ae92bf6.js
@@ -0,0 +1,74 @@
+self.__precacheManifest = [
+  {
+    "revision": "6cc3ba820e0d16d30c25",
+    "url": "/Amazon-Clone/static/css/main.7d711b6a.chunk.css"
+  },
+  {
+    "revision": "6cc3ba820e0d16d30c25",
+    "url": "/Amazon-Clone/static/js/main.6cc3ba82.chunk.js"
+  },
+  {
+    "revision": "a94284e4e9fa8c990d06",
+    "url": "/Amazon-Clone/static/js/1.a94284e4.chunk.js"
+  },
+  {
+    "revision": "2ea162389c15005c206c",
+    "url": "/Amazon-Clone/static/js/runtime~main.2ea16238.js"
+  },
+  {
+    "revision": "80775cf681f17a79015d6f671b975cf0",
+    "url": "/Amazon-Clone/static/media/amazon-logo-dark.80775cf6.png"
+  },
+  {
+    "revision": "dc2ade83b2fd1eda6bcaf42869cfc0f4",
+    "url": "/Amazon-Clone/static/media/america-flag.dc2ade83.png"
+  },
+  {
+    "revision": "f1b7fec6c618702caaa9a8aecf733608",
+    "url": "/Amazon-Clone/static/media/amazon-logo.f1b7fec6.png"
+  },
+  {
+    "revision": "98c9c4b9ddb556bf4aea241df39e818a",
+    "url": "/Amazon-Clone/static/media/cart.98c9c4b9.png"
+  },
+  {
+    "revision": "01c3525fa36e4ea4464e6c2fee84424a",
+    "url": "/Amazon-Clone/static/media/home-bg.01c3525f.jpg"
+  },
+  {
+    "revision": "3db7d8fd75c4d583f5698e287f13591b",
+    "url": "/Amazon-Clone/static/media/home-bg-2.3db7d8fd.jpg"
+  },
+  {
+    "revision": "8a71dc804760004bbac2f90d2822f1e3",
+    "url": "/Amazon-Clone/static/media/product-iphone13.8a71dc80.png"
+  },
+  {
+    "revision": "7d1fd216fe62e47036a7fb6327b10cd3",
+    "url": "/Amazon-Clone/static/media/product-samsungs21.7d1fd216.png"
+  },
+  {
+    "revision": "4d770b9ebbd715de3e65e0d7e506fa5b",
+    "url": "/Amazon-Clone/static/media/product-amazonecho.4d770b9e.png"
+  },
+  {
+    "revision": "2776d904deb79f69556abe11f37c86b9",
+    "url": "/Amazon-Clone/static/media/product-ipadpro.2776d904.png"
+  },
+  {
+    "revision": "2114fa8de83ca58e901d826bc9011857",
+    "url": "/Amazon-Clone/static/media/product-applewatch.2114fa8d.png"
+  },
+  {
+    "revision": "0c3b62b509401a2ce2d6dc9dc8afe240",
+    "url": "/Amazon-Clone/static/media/product-samsungtv.0c3b62b5.png"
+  },
+  {
+    "revision": "1db901c259ebf2345220689f01500802",
+    "url": "/Amazon-Clone/static/media/Developer-Ad.1db901c2.png"
+  },
+  {
+    "revision": "960b58a311f416ffa10ec679fd5baec6",
+    "url": "/Amazon-Clone/index.html"
+  }
+];
